57786841547303a33a50edec05ad3b966305738f
WebCoderCK <80730777+WebCoderCK@users.noreply.github.com>
2021-10-03 11:00:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function addition(){
+    var a= Number(document.getElementById("FirstNumber").value)+
+        Number(document.getElementById("SecondNumber").value);
+    window.alert(a);
+  }
+  function subtraction(){
+    var b= Number(document.getElementById("FirstNumber").value)-
+        Number(document.getElementById("SecondNumber").value);
+    window.alert(b);
+  }
+  function multiplication(){
+    var c= Number(document.getElementById("FirstNumber").value)*
+        Number(document.getElementById("SecondNumber").value);
+    window.alert(c);
+  }
+  function division(){
+    var d= Number(document.getElementById("FirstNumber").value)/
+        Number(document.getElementById("SecondNumber").value);
+    window.alert(d);
+  }
